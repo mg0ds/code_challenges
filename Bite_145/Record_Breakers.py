d6b872338282b472b7beac29347ca3e45220a793
mg0ds <53785137+mg0ds@users.noreply.github.com>
2022-11-26 21:56:38 +0100

    Create Record_Breakers.py

---
+++ Bite_145/Record_Breakers.py
@@ -0,0 +1,54 @@
+from collections import namedtuple
+from datetime import date
+
+import pandas as pd
+
+DATA_FILE = "https://bites-data.s3.us-east-2.amazonaws.com/weather-ann-arbor.csv"
+STATION = namedtuple("Station", "ID Date Value")
+
+
+def high_low_record_breakers_for_2015():
+    """Extract the high and low record breaking temperatures for 2015
+
+    The expected value will be a tuple with the highest and lowest record
+    breaking temperatures for 2015 as compared to the temperature data
+    provided.
+
+    NOTE:
+    The date values should not have any timestamps, should be a
+    datetime.date() object. The temperatures in the dataset are in tenths
+    of degrees Celsius, so you must divide them by 10
+
+    Possible way to tackle this challenge:
+
+    1. Create a DataFrame from the DATA_FILE dataset.
+
+    2. Manipulate the data to extract the following:
+       * Extract highest temperatures for each day / station pair between 2005-2015
+       * Extract lowest temperatures for each  day / station  between 2005-2015
+       * Remove February 29th from the dataset to work with only 365 days
+
+    3. Separate data into two separate DataFrames:
+       * high/low temperatures between 2005-2014
+       * high/low temperatures for 2015
+
+    4. Iterate over the 2005-2014 data and compare to the 2015 data:
+       * For any temperature that is higher/lower in 2015 extract ID,
+         Date, Value
+         
+    5. From the record breakers in 2015, extract the high/low of all the
+       temperatures
+       * Return those as STATION namedtuples, (high_2015, low_2015)
+    """
+    df = pd.read_csv(DATA_FILE)
+    pd.options.mode.chained_assignment = None  # ignore warnings
+    df["Data_Value"] = df["Data_Value"].divide(10)
+    
+    df2015 = df[(df['Date'] > '2015-01-01') & (df['Date'] < '2015-12-31')]
+    df2015S = df2015.nsmallest(1, "Data_Value")
+    df2015L = df2015.nlargest(1, "Data_Value")
+    high_2015 = STATION(df2015L["ID"].array, df2015L["Date"].array, df2015L["Data_Value"].array)
+    low_2015 = STATION(df2015S["ID"].array, df2015S["Date"].array, df2015S["Data_Value"].array)
+    bbbb = (high_2015, low_2015)
+    
+    return bbbb
