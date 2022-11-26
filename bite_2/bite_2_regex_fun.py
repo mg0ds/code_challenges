04fd5fe620301a81835ee49ff849a7fab94c7f37
mg0ds <53785137+mg0ds@users.noreply.github.com>
2022-11-26 20:43:59 +0100

    Create bite_2_regex_fun.py

---
+++ bite_2/bite_2_regex_fun.py
@@ -0,0 +1,54 @@
+import re
+
+
+COURSE = ('Introduction 1 Lecture 01:47'
+          'The Basics 4 Lectures 32:03'
+          'Getting Technical!  4 Lectures 41:51'
+          'Challenge 2 Lectures 27:48'
+          'Afterword 1 Lecture 05:02')
+TWEET = ('New PyBites article: Module of the Week - Requests-cache '
+         'for Repeated API Calls - http://pybit.es/requests-cache.html '
+         '#python #APIs')
+HTML = ('<p>pybites != greedy</p>'
+        '<p>not the same can be said REgarding ...</p>')
+
+
+def extract_course_times(course=COURSE):
+    """Return the course timings from the passed in
+       course string. Timings are in mm:ss (minutes:seconds)
+       format, so taking COURSE above you would extract:
+       ['01:47', '32:03', '41:51', '27:48', '05:02']
+       Return this list.
+    """
+    times = re.findall(r"[\d:]+", course)
+    list = []
+    for a in times:
+        if len(a) > 2:
+            list.append(a)
+    return list
+
+def get_all_hashtags_and_links(tweet=TWEET):
+    """Get all hashtags and links from the tweet text
+       that is passed into this function. So for TWEET
+       above you need to extract the following list:
+       ['http://pybit.es/requests-cache.html',
+        '#python',
+        '#APIs']
+       Return this list.
+    """
+    wiersz = re.split(r"\s", tweet)
+    list = []
+    for w in wiersz:
+        if re.match(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", w) != None or re.match(r"(#\w+)", w):
+            list.append(w)
+    return list
+
+
+def match_first_paragraph(html=HTML):
+    """Extract the first paragraph of the passed in
+       html, so for HTML above this would be:
+       'pybites != greedy' (= content of first paragraph).
+       Return this string.
+    """
+    a = re.split(r"</p>", html)[0]
+    return a.replace("<p>", "")
